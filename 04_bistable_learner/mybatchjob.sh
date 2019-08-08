f4b34ea50e19abb479157142701860a0a576afce
Gui Miotto <guilherme.miotto@gmail.com>
2019-08-08 20:07:40 +0200

    new nemo runs with Aminus=0

--- 04_bistable_learner/mybatchjob.sh
+++ 04_bistable_learner/mybatchjob.sh
@@ -6,9 +6,9 @@
 ##MSUB -l naccesspolicy=singlejob
 #MSUB -m bea -M alessang@tf.uni-freiburg.de
 #MSUB -v MPIRUN_OPTIONS="--bind-to core --map-by core -report-bindings"
-#MSUB -N run_03
+#MSUB -N run_04
 
-TASKNAME=run_03
+TASKNAME=run_04
 SCRIPTPATH=$HOME/code/$TASKNAME.py
 OUTDIR=$(ws_find learner)/$TASKNAME
 
@@ -16,4 +16,4 @@ module load system/modules/testing
 module load mpi/openmpi/3.1-gnu-7.3
 module load neuro/nest/2.16.0-python-3.7.0
 
-mpirun python $SCRIPTPATH $OUTDIR --n_trials 200
+mpirun python $SCRIPTPATH $OUTDIR --n_trials 300
---
+++ 04_bistable_learner/run_04.py
@@ -0,0 +1,26 @@
+import sys, argparse
+from Experiment import Experiment
+
+if __name__ == '__main__':
+    # Parse command line input parameters
+    parser = argparse.ArgumentParser(description='Run one experiment once')
+    parser.add_argument('outdir', type=str, help='Output folder for results')
+    parser.add_argument('--n_trials', default=400, type=int,
+        help='Number of trials in the experiment (default 400)')
+    args = parser.parse_args()
+    
+    # Build experiment
+    exp = Experiment()
+    
+    # Tweeks go here:
+    aversion=True
+    exp.brain.striatum.w = 1.
+    exp.brain.striatum.J_intra = exp.brain.striatum.w * exp.brain.striatum.J_inter
+    exp.brain.vta.DA_pars['A_minus'] = 0.
+
+
+    # Run experiment
+    exp.train_brain(n_trials=args.n_trials, aversion=aversion, save_dir=args.outdir)
+
+
+    
